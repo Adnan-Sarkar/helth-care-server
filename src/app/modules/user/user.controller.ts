5f65ea55ba5b89624194f795af7ccb89c3760f22
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-03-27 17:12:19 +0600

    Handle error using try-catch in controller

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,9 +3,21 @@ import { userServices } from "./user.service";
 
 // create admin
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userServices.createAdmin(req.body);
+  try {
+    const result = await userServices.createAdmin(req.body);
 
-  res.send(result);
+    res.status(201).json({
+      success: true,
+      message: "Admin created successfuly",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Something went wrong!",
+      error,
+    });
+  }
 };
 
 export const userControllers = {
