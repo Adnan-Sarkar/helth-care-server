00b486e26b8231f1dbc3938c6c807d87f2b2a714
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-03-27 07:09:51 +0600

    Add route in app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application } from "express";
 import cors from "cors";
 import exp from "constants";
+import { userRoutes } from "./app/modules/user/user.routes";
 
 const app: Application = express();
 
+// middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send({
