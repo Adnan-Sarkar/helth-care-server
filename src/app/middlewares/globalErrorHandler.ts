3afdc53a0dc11c0118f847db1a9fbab37e902414
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-03-29 03:41:42 +0600

    Create global error handler middleware

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+
+const globalErrorHandler = (
+  error: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+    success: false,
+    message: error?.name || "Something went wrong!",
+    error,
+  });
+};
+
+export default globalErrorHandler;
