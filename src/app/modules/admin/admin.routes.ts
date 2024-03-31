27eed2680ccc81e11f58e47bb4cd42bcab545aa8
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-03-31 15:53:12 +0600

    Add auth guard to implement authorization in admin route

--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -2,21 +2,31 @@ import express from "express";
 import { adminControllers } from "./admin.controller";
 import validateRequet from "../../middlewares/validateRequest";
 import { adminValidations } from "./admin.validation";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
-router.get("/", adminControllers.getAllAdmins);
+router.get("/", auth("SUPER_ADMIN", "ADMIN"), adminControllers.getAllAdmins);
 
-router.get("/:id", adminControllers.getAdminById);
+router.get("/:id", auth("SUPER_ADMIN", "ADMIN"), adminControllers.getAdminById);
 
 router.patch(
   "/:id",
+  auth("SUPER_ADMIN", "ADMIN"),
   validateRequet(adminValidations.updateValidationSchema),
   adminControllers.updateAdminById
 );
 
-router.delete("/:id", adminControllers.deleteAdmin);
+router.delete(
+  "/:id",
+  auth("SUPER_ADMIN", "ADMIN"),
+  adminControllers.deleteAdmin
+);
 
-router.delete("/soft/:id", adminControllers.softDeleteAdmin);
+router.delete(
+  "/soft/:id",
+  auth("SUPER_ADMIN", "ADMIN"),
+  adminControllers.softDeleteAdmin
+);
 
 export const adminRoutes = router;
