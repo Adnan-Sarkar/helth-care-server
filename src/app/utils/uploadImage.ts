e96f59d82e4200ba24fcea42b59d49c71133849a
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-04-02 06:22:58 +0600

    Create uploadImage utility function using multer

---
+++ src/app/utils/uploadImage.ts
@@ -0,0 +1,15 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (_req, _file, cb) {
+    cb(null, path.join(process.cwd(), "/uploads"));
+  },
+  filename: function (_req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const uploadImage = multer({ storage: storage });
+
+export default uploadImage;
